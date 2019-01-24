e6bf83ab8f2315f48e14c0f8c7669fde7ec10e32
shuaizhitao <shuaizhitao@126.com>
2019-01-24 15:23:23 +0800

    初始化项目

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by SZT on 2019/1/24.
+//  Copyright © 2019年 WinMo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by SZT on 2019/1/24.
+//  Copyright © 2019年 WinMo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
